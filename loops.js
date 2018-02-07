f9ea6ef01cd2b8e4271e02a9e2c0507d7fc55b8c
Pawan M <pawanmurthy92@gmail.com>
2018-02-07 03:57:50 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,9 +2,14 @@ function forLoop(array){
   for(i=0;i<25;i++){
     if(i<=1)
     {
-      array.push()
+      array.push("I am 1 strange loop.");
+    }
+    else
+    {
+      array.push(`I am ${i} loop.`);
     }
     
   }
+  return array;
   
 }
